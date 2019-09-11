5c0f64823c055ef38ef11a9cede4fdf87625c839
leon <156555189@qq.com>
2019-09-11 23:42:57 +0800

    9月11日 晚上打卡

--- src/main.js
+++ src/main.js
@@ -1,20 +1,98 @@
+// 导入css
+import "swiper/dist/css/swiper.min.css";
+import "./css/animate.min.css";
+import "./css/index.scss";
+
+// 导入js
 import $ from "jquery";
-import Swiper from "swiper";
-import Vue from "vue";
-import "swiper/dist/css/swiper.css";
-import "./css/index.css"
+import swiper from "swiper";
 
-import App from "./App.vue";
+var mySwiper = new swiper(".swiper-container", {
+  direction: "vertical",
+  initialSlide: 0,
+  // effect: "fade",
 
-new Vue({
-  el: "#app",
-  data:{},
-  render: c => c(App),
-    mounted(){
-      var mySwiper = new Swiper('.swiper-container',{
-        direction: "vertical",
-        speed:3000
-      })
-  
+  on: {
+    init() {
+      swiperAnimateCache(this); //隐藏动画元素
+      swiperAnimate(this); //初始化完成开始动画
     },
+    slideChangeTransitionEnd: function() {
+      swiperAnimate(this);
+    },
+    slideChange: function() {
+      $(".gezi").css("left", "30px");
+      $(".role3").css("right", "-7rem");
+      $(".car2").css("left", "-15rem");
+      $(".think2").hide();
+      $(".content2").hide();
+      $(".mark1").hide();
+      $(".mark2").hide();
+      $(".mark3").hide();
+      $(".think3").hide();
+      $(".content3").hide();
+      $(".aside3").hide();
+      $(".role2-3").css({ top: "25rem", left: "-4rem" });
+      switch (this.activeIndex) {
+        case 1:
+        case 2:
+          $(".car2")
+            .stop()
+            .animate({ left: "0rem" }, 200, () => {
+              $(".think2")
+                .stop()
+                .show(1000, () => {
+                  $(".content2")
+                    .stop()
+                    .show(0, () => {
+                      $(".mark1")
+                        .stop()
+                        .show(100, () => {
+                          $(".mark2")
+                            .stop()
+                            .show(1000, () => {
+                              $(".role2-3")
+                                .stop()
+                                .animate(
+                                  { left: "0rem", top: "20rem" },
+                                  1500,
+                                  () => {
+                                    $(".mark3")
+                                      .stop()
+                                      .show(0);
+                                  }
+                                );
+                            });
+                        });
+                    });
+                });
+            });
+        case 3:
+          $(".role3")
+            .stop()
+            .animate({ right: "0rem" }, 1000, () => {
+              $(".think3")
+                .stop()
+                .show(100, () => {
+                  $(".content3")
+                    .stop()
+                    .show(1000, () => {
+                      $(".aside3")
+                        .stop()
+                        .show(200);
+                    });
+                });
+            });
+
+        case 4:
+          $(".gezi").animate(
+            {
+              left: "+300px"
+            },
+            1000
+          );
+        case 5:
+      }
+    }
+  }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 
 module.exports = {
@@ -14,13 +13,11 @@ module.exports = {
       template: path.join(__dirname, "src", "index.html"),
       filename: "index.html"
     }),
-    new VueLoaderPlugin()
   ],
   module: {
     rules: [
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
       { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
-      { test: /\.vue$/, use: "vue-loader" },
       {
         test: /\.(jpg|png|jpeg|bmp|gif)$/,
         use: ["url-loader?limit=1000&name=[hash:8]-[name].[ext]"]
@@ -29,12 +26,12 @@ module.exports = {
         test: /\.(ttf|woff2|woff|eot|svg)$/,
         use: "url-loader"
       },
-      // {
-      //   test: /\.js$/,
-      //   use: "babel-loader",
-      //   //  排除node_modules中的js
-      //   exclude: /node_modules/
-      // }
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        //  排除node_modules中的js
+        exclude: /node_modules/
+      }
     ]
   }
 };
