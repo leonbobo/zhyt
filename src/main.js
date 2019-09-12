e54c6367a9d007b2804cff9218b466e10bd210e6
leon <156555189@qq.com>
2019-09-13 06:11:12 +0800

    9月13号 早上结束战斗

--- src/main.js
+++ src/main.js
@@ -7,92 +7,122 @@ import "./css/index.scss";
 import $ from "jquery";
 import swiper from "swiper";
 
-var mySwiper = new swiper(".swiper-container", {
+// 第一模块
+load()
+function load(){
+  setTimeout(()=>{
+    oneSwiper.init()
+    $(".load-container").hide()
+  },2000)
+}
+var oneSwiper = new swiper(".swiper-one", {
   direction: "vertical",
+  init: false,
   initialSlide: 0,
-  // effect: "fade",
-
+  // effect: "slide",
+  speed: 500,
+  lazy: {
+    loadPrevNext: true,
+  },
   on: {
     init() {
       swiperAnimateCache(this); //隐藏动画元素
       swiperAnimate(this); //初始化完成开始动画
+      this.allowSlidePrev = false;
     },
+    slideChangeTransitionStart() {},
     slideChangeTransitionEnd: function() {
       swiperAnimate(this);
+      if ((this.activeIndex != 0) | 2) this.allowSlidePrev = true;
+      else this.allowSlidePrev = false;
     },
-    slideChange: function() {
-      $(".gezi").css("left", "30px");
-      $(".role3").css("right", "-7rem");
-      $(".car2").css("left", "-15rem");
-      $(".think2").hide();
-      $(".content2").hide();
-      $(".mark1").hide();
-      $(".mark2").hide();
-      $(".mark3").hide();
-      $(".think3").hide();
-      $(".content3").hide();
-      $(".aside3").hide();
-      $(".role2-3").css({ top: "25rem", left: "-4rem" });
-      switch (this.activeIndex) {
-        case 1:
-        case 2:
-          $(".car2")
-            .stop()
-            .animate({ left: "0rem" }, 200, () => {
-              $(".think2")
-                .stop()
-                .show(1000, () => {
-                  $(".content2")
-                    .stop()
-                    .show(0, () => {
-                      $(".mark1")
-                        .stop()
-                        .show(100, () => {
-                          $(".mark2")
-                            .stop()
-                            .show(1000, () => {
-                              $(".role2-3")
-                                .stop()
-                                .animate(
-                                  { left: "0rem", top: "20rem" },
-                                  1500,
-                                  () => {
-                                    $(".mark3")
-                                      .stop()
-                                      .show(0);
-                                  }
-                                );
-                            });
-                        });
-                    });
-                });
-            });
-        case 3:
-          $(".role3")
-            .stop()
-            .animate({ right: "0rem" }, 1000, () => {
-              $(".think3")
-                .stop()
-                .show(100, () => {
-                  $(".content3")
-                    .stop()
-                    .show(1000, () => {
-                      $(".aside3")
-                        .stop()
-                        .show(200);
-                    });
-                });
-            });
+    slideChange: function() {}
+  }
+});
 
-        case 4:
-          $(".gezi").animate(
-            {
-              left: "+300px"
-            },
-            1000
-          );
-        case 5:
-      }
+// 第2模块
+
+var twoSwiper = new swiper(".swiper-two", {
+  direction: "horizontal",
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 0,
+    depth: 70,
+    modifier: 0.6,
+    slideShadows: false
+  },
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween : 10,
+  centeredSlides: true,
+  slideToClickedSlide: true,
+  noSwiping: false,
+  
+  on: {
+    init() {
+    },
+    slideChangeTransitionStart() {
+    },
+    slideChangeTransitionEnd: function() {},
+    slideChange: function() {
+      var slideIndex = (this.activeIndex % 4) + 1;
+      console.log(slideIndex);
+      var imgPath = "url('./images/page5/封面" + slideIndex + ".png') no-repeat";
+      $(".page5 .text-container").css({
+        background: imgPath,
+        "background-size": "100%"
+      });
+      
     }
   }
 });
+
+$(".sign").on("change", function() {
+  this.value = "— " + this.value.replace("— ", "");
+});
+$(".makepic").on("click", () => {
+  var toIndex = (twoSwiper.activeIndex % 4) + 5;
+  oneSwiper.slideTo(toIndex);
+});
+
+$("#start_btn").on("click",() => {
+  oneSwiper.slideNext(0);
+  twoSwiper.init();
+});
+
+// 阻止页面被拖动
+document.addEventListener(
+  "touchmove",
+  function(e) {
+    e.preventDefault();
+  },
+  isPassive()
+    ? {
+        capture: false,
+        passive: false
+      }
+    : false
+);
+document.body.addEventListener(
+  "touchmove",
+  function(e) {
+    e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+  },
+  { passive: false }
+); //passive 参数不能省略，用来兼容ios和android
+function isPassive() {
+  var supportsPassiveOption = false;
+  try {
+    addEventListener(
+      "test",
+      null,
+      Object.defineProperty({}, "passive", {
+        get: function() {
+          supportsPassiveOption = true;
+        }
+      })
+    );
+  } catch (e) {}
+  return supportsPassiveOption;
+}
